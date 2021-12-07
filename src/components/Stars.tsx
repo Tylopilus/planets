5e35842868c6ceee47cb0605e0563fd99f387104
Helge Ahrens <helgeahrens@MacBook-Pro.local>
2021-12-08 00:32:21 +0100

    finished main page

--- src/components/Stars.tsx
+++ src/components/Stars.tsx
@@ -4,16 +4,18 @@ import { createMouseToElement } from '@solid-primitives/mouse';
 export function Stars(props) {
   const c = children(() => props.children);
   const [{ x, y, width, height, isInside }] = createMouseToElement(
-    () => (c() as Element).children[0]
+    // () => (c() as Element).children[0]
+    () => globalThis.document.querySelector('main')
   );
   createEffect(() => {
-    const offsetX = ((x() - width() / 2) / width()) * 2;
-    const offsetY = ((y() - height() / 2) / height()) * 2;
+    const offsetX = (((x() - width() / 2) / width()) * 2) % 1;
+    const offsetY = (((y() - height() / 2) / height()) * 2) % 1;
+    console.log(offsetX, offsetY);
     const elem = [
       (c() as HTMLElement).children[0] as HTMLElement,
       (c() as HTMLElement).children[1] as HTMLElement,
     ];
-    if (isInside() && x()) {
+    if (x()) {
       for (const el of elem) {
         el.style.setProperty('--offsetX', offsetX.toString());
         el.style.setProperty('--offsetY', offsetY.toString());
